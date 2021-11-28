f552bfbe5eb5497e731e19be8751ca23c6d39140
Wasanta <kwan_u_day@hotmail.com>
2021-11-28 15:49:46 -0500

    repeat done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,52 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  // const [firstName, setFirstName] = useState("John");
+  // const [lastName, setLastName] = useState("Henry");
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry" 
+  })
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
+  //make the handler to use in many name and value//by seting name attribute in input
+  function handleChange(event){
+    // name is the KEY in of the formData object we're trying to update
+    const name = event.target.name;
+    const value = event.target.value;
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
+    setFormData({
+      ...formData,
+      [name]:value
+    })
   }
+  
+
+  // function handleFirstNameChange(event) {
+  //   setFormData({
+  //     ...formData,
+  //     firstName: event.target.value
+  //   });
+  // }
+
+  // function handleLastNameChange(event) {
+  //   setFormData({
+  //     ...formData,
+  //     lastName: event.target.value
+  //   });
+  // }
 
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+      <input 
+        type="text" 
+        name="firstName"
+        onChange={handleChange}  
+        value={formData.firstName} />
+      <input 
+        type="text" 
+        name="lastName"
+        onChange={handleChange} 
+        value={formData.lastName} />
       <button type="submit">Submit</button>
     </form>
   );
