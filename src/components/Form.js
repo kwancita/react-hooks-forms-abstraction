63f26cea871ee5c3d61449367ccb0c78a90f1c7b
Wasanta <kwan_u_day@hotmail.com>
2021-11-28 15:30:43 -0500

    Revert "added"
    This reverts commit 72c5cc55e924a7239d831560b4f183d402d37be3.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,45 +1,21 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [formData, setFormData] = useState({
-    firstName: "Johny",
-    lastName: "Henry",
-    admin: false
-  })
+  const [firstName, setFirstName] = useState("John");
+  const [lastName, setLastName] = useState("Henry");
 
-  function handleChange(event){
-    const name = event.target.name
-    let value = event.target.value
-
-    if (event.target.type === "checkbox"){
-        value = event.target.cheked;
-    }
-
-    setFormData({
-      ...formData,
-      [name]:value
-    })
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
   }
- 
-  // function handleSubmit(event){
-  //   event.preventDefault();
-  //   console.log(formData)
-  // }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
 
   return (
     <form>
-      <input type="text" 
-        name="firstName"
-        onChange={handleChange} 
-        value={formData.firstName} />
-      <input type="text"  
-        name="lastName"
-        onChange={handleChange} 
-        value = {formData.lastName} />
-      <input type="checkbox"  
-        name="admin"
-        onChange={handleChange} 
-        value = {formData.admin} />
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
